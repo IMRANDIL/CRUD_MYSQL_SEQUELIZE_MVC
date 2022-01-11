bee9cbf1f1a5198fef737b1ac9a6c5c4f26d5b7c
IMRANDIL <aliimranadil2@gmail.com>
2022-01-11 18:24:26 +0530

    yh

---
+++ controllers/ProductController.js
@@ -0,0 +1,107 @@
+const { where } = require('sequelize/dist');
+const db = require('../models');
+
+
+
+//create main model...
+
+const Product = db.products;
+
+const Review = db.reviews;
+
+//main action starts here..
+
+//1.Create Product...
+
+const addProduct = async (req, res) => {
+    try {
+        let info = {
+            title: req.body.title,
+            price: req.body.price,
+            description: req.body.description,
+            published: req.body.published ? req.body.published : false
+
+        }
+
+        const product = await Product.create(info);
+        res.status(200).send(product);
+    } catch (error) {
+        console.log(error);
+    }
+
+}
+
+
+//2.Get All Products...
+
+
+
+const getAllProduct = async (req, res) => {
+    try {
+        // const products = await Product.findAll({
+        //     attributes: [
+        //         'title',
+        //         'price',
+        //         'description'
+        //     ]
+        // });
+
+        const products = await Product.findAll({});
+        res.status(200).send(products);
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+//3.Get Single Product.....
+
+
+const getSpecProduct = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const specProduct = await Product.findOne({ where: { id: id } });
+        res.status(200).send(specProduct);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+//4.Update Product....
+
+const updateSpecProduct = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const specProduct = await Product.update(req.body, { where: { id: id } });
+        await specProduct.save();
+        res.status(200).send(`successfully edited..`);
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+//5.Delete Specific Product....
+
+
+const deleteSpecProduct = async (req, res) => {
+    try {
+        const { id } = req.params;
+        await Product.destroy({ where: { id: id } });
+        res.status(200).send(`Product Id:${id} deleted..`)
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+module.exports = {
+    addProduct,
+    getAllProduct,
+    getSpecProduct,
+    updateSpecProduct,
+    deleteSpecProduct
+}
